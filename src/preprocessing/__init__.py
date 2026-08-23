d13f947331027ab94ccfd9ca3470a690fe50a27f
SsakuraA1 <2281849219@qq.com>
2026-08-23 10:31:50 +0800

    data: add corpus validation tooling

---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities for validating and loading raw corpus documents."""
+
+from src.preprocessing.corpus import CorpusDocument, CorpusValidationError, load_corpus
+
+__all__ = ["CorpusDocument", "CorpusValidationError", "load_corpus"]
