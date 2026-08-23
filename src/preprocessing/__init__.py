97cee0c3e015df574351c7d984aa1270e5df84a8
SsakuraA1 <2281849219@qq.com>
2026-08-23 12:27:01 +0800

    Revert "Merge pull request #1 from diongJ/feature/data-corpus"
    This reverts commit 94b0577c5f7fde9445f1cf013e307e14b04cedc0, reversing
    changes made to 3191e2ff1f6efaa459b0efde97a1bce8bbaf5281.

--- src/preprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities for validating and loading raw corpus documents."""
-
-from src.preprocessing.corpus import CorpusDocument, CorpusValidationError, load_corpus
-
-__all__ = ["CorpusDocument", "CorpusValidationError", "load_corpus"]
